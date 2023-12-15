d6b4e3df1a70458ec960738b1b56b832c363b528
Nikita Vlasov <84715117+NikitaVlas@users.noreply.github.com>
2023-12-15 10:33:41 +0100

    feat(server): DB config

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
+
+//Constans
+const PORT = process.env.PORT || 3001
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
 
 async function start() {
     try {
         await mongoose.connect(
-            'mongodb+srv://wgwizard:wgwizard@cluster0.dwkadbg.mongodb.net/wg-wizard?retryWrites=true&w=majority'
+            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.dwkadbg.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
         )
 
-        app.listen(3002, () => console.log(`Server started on port: ${3002}`))
+        app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
     } catch (error) {
         console.log(error)
     }
