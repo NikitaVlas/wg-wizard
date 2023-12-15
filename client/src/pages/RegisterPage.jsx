213a1d664cef3406051a6f40d19d5e5c0d367e41
Nikita Vlasov <84715117+NikitaVlas@users.noreply.github.com>
2023-12-15 15:03:28 +0100

    feat(client): RegisterPage ui

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const RegisterPage = () => {
     return (
-        <div>
-            REGISTER
+        <div className="container mt-5">
+            <div className="card p-4" style={{ maxWidth: "400px", margin: "auto" }}>
+                <form
+                    onSubmit={e => e.preventDefault()}
+                    className="text-center"
+                >
+                    <div className="mt-4">
+                        <h1>Registration</h1>
+
+                        <div className="form-group mt-3">
+                            <label htmlFor="username">User name</label>
+                            <input
+                                type="text"
+                                id="username"
+                                placeholder="User name"
+                                className="form-control mt-2"
+                            />
+                        </div>
+
+                        <div className="form-group mt-3">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                                className="form-control mt-2"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="d-flex justify-content-center mt-4">
+                        <button type="submit" className="btn btn-primary mr-2 me-2">Confirm</button>
+                        <Link to={"/login"} className="btn btn-secondary ms-2">Already registered?</Link>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
